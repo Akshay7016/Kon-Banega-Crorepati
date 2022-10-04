ed27ff82a78dc646888e2be017249d686703bcc0
Akshay7016 <asajgule@gmail.com>
2022-10-04 18:01:59 +0530

    animation added to correct and wrong answer

---
+++ src/components/Trivia.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Trivia = () => {
+    return (
+        <div className='trivia'>
+            <div className='question'>What is your name</div>
+            <div className='answers'>
+                <div className='answer wrong'>Akshay</div>
+                <div className='answer'>Akshay</div>
+                <div className='answer'>Akshay</div>
+                <div className='answer'>Akshay</div>
+            </div>
+        </div>
+    )
+}
+
+export default Trivia
